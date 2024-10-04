92ecb3d95c3f154cd4587b9cb2bf76081214195f
Largerest <largerest@gmail.com>
2024-10-03 20:42:37 -0500

    Update scanRepo.js

--- scanRepo.js
+++ scanRepo.js
@@ -16,13 +16,11 @@ function getDirectoryStructure(dir) {
             structure[file] = null;
         }
     });
-
+        
+    delete directoryStructure['.git'];
+    delete directoryStructure['ull'];
     return structure;
 }
 
 const repoPath = path.resolve(__dirname, '.'); // Set this to the root of your repo
-const structure = getDirectoryStructure(repoPath);
-fs.writeFileSync('directory_structure.json', JSON.stringify(structure, null, 2));
-
-console.log('Directory structure saved to directory_structure.json');
-console.log(structure);
+console.log(getDirectoryStructure(repoPath));
